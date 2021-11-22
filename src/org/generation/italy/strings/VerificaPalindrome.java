5f424e8eb569f7a2f23c5d06da53aa5ca6e4f4e6
RobertaAngioni <robertaangioni92@gmail.com>
2021-11-22 14:25:15 +0100

    secondo commit

--- src/org/generation/italy/strings/VerificaPalindrome.java
+++ src/org/generation/italy/strings/VerificaPalindrome.java
@@ -7,10 +7,10 @@ public class VerificaPalindrome {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		
-		System.out.println("Inserici una parola: ");
+		System.out.print("Inserici una parola: ");
 		String palindromeWord = scan.nextLine();
 		String reversePalindrome = "";
-		
+		int count = 0;
 		
 		for(int i = palindromeWord.length()-1; i >= 0; i--) {
 			
@@ -20,16 +20,19 @@ public class VerificaPalindrome {
 			
 		}
 		if(palindromeWord.equals(reversePalindrome)) {
-			System.out.println("This word is a palindrome");
+			System.out.println("This word is a palindrome and it counts " + palindromeWord.length() + " letters");
 		}else {
-			System.out.println("This word is not a palindrome");
+			System.out.println("This word is not a palindrome and it counts " + palindromeWord.length() + " letters");
 		}
 		
 		
 		
 		
-		
-		
+//	    for(int j = 0; j < palindromeWord.length(); j++) {
+//	    	if(palindromeWord.charAt(j) != " ")
+//	    }
+//		
+//		
 		
 		
 		
